f582dccaaa3ae1016b20eac114defa75aec8332c
Jessica Choi <jessica@Jessicas-MacBook-Air.local>
2019-05-28 20:36:41 -0700

    add sorting ability to wine catalogue

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -2,8 +2,19 @@ var Wine = require('../models/wine');
 var Partner = require('../models/partner');
 
 module.exports = {
-  create
+  create,
+  deleteReview
 };
+
+function deleteReview(req, res, next) {
+  Wine.findById(req.params.wineId, function(err, wine) {
+    wine.reviews.id(req.params.reviewId).remove();
+    wine.save(function(err) {
+      res.redirect(`/wines/${wine._id}`)
+    });
+  });
+}
+
 
 function create(req, res) {
   Wine.findById(req.params.id, function (err, wine) {
--- routes/reviews.js
+++ routes/reviews.js
@@ -3,5 +3,6 @@ var router = express.Router();
 var reviewsController = require('../controllers/reviews');
 
 router.post('/wines/:id/reviews', reviewsController.create);
+router.delete('/:wineId/:reviewId', reviewsController.deleteReview);
 
 module.exports = router;
